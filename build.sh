2414a912264716073bd170d009970d4542aa27d9
Alex <alex@Vantages-MacBook-Pro.local>
2019-04-07 12:14:20 -0700

    Added ios relevent files

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #/bin/bash
+export POLLY_IOS_DEVELOPMENT_TEAM="BCZNDS34S5"
+
 rm -rf build
 mkdir build
 cd build 
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../../polly/ios-11-0.cmake -GXcode
+pwd
+cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake_files/ios_cmake.cmake -GXcode
 make
