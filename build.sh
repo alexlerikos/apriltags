7f2220e1561ec2a218a79612b638d4cd615aa5e7
Alex Lerikos <alerikos@gmail.com>
2019-04-07 21:19:47 -0700

    Updated cmake lists file to build xcode framework project for ios integration

--- build.sh
+++ build.sh
@@ -5,5 +5,5 @@ rm -rf build
 mkdir build
 cd build 
 pwd
-cmake .. -DCMAKE_BUILD_TYPE=Debug -DCMAKE_TOOLCHAIN_FILE=../cmake_files/ios_cmake.cmake -GXcode
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../cmake_files/ios_cmake.cmake -GXcode
 make
